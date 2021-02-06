70cfe60898e2cbf59ee7b8340df187dbc21add27
CSMaus <cs-maus-1@mail.ru>
2021-02-07 01:13:29 +0300

    ошибка C2676

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@
 #include <unordered_set>
 #include <exception>
 #include <unordered_map>
-#include "ConsoleApplication1.h"
+//#include "ConsoleApplication1.h"
 
 using namespace::std;
 
@@ -54,32 +54,21 @@ bool put(Grid& grid, int x, int y, bool it)
     //return it->second;
 }
 
-
-int main()
+bool random_decision(float Z, float n, float z1) //float &T[k][l]
 {
-    cout << "Hello World!\n";
 
-    //Grid Lig;
-    //int d = get(Lig, 4, 6);
-    //put(Lig, 5, 6, true); //тест работает ли
-    
+    float psi = Z * rand();
+    float Z1 = pow(z1, n);
+    //float Z1 = pow((get(T, k - 1, l) + get(T, k + 1, l) + get(T, k, l - 1) + get(T, k, l + 1)), n);
+    //float Z1 = pow((*T[k - 1][l] + *T[k + 1][l]  + *T[k][l - 1] + *T[k][l + 1]), n);
 
-    float n = 0.95;
-
-
-
-    for (int i = 0; i < lenX; i++) {
-        cout << " \n";
-        for (int j = 0; j < lenX; j++) {
-            cout << get(Lig, i, j) + " ";
-        }
-
-    }
-
+    return Z1 > psi;
 }
 
-Grid& generate_field(float n, Grid& grid) //было int вместо Grid& (magic  @_@ )
+Grid* generate_field(float n, Grid& grid) //было int вместо Grid& (magic  @_@ )
 {
+    Grid Lig;
+
     // Set Boundary condition for lighthing
     int Ttop = 45;  // 100
     int Tbottom = 0; // 0
@@ -87,38 +76,37 @@ Grid& generate_field(float n, Grid& grid) //было int вместо Grid& (mag
     int Tright = 15;  // 30
     int Tguess = 5; //30
 
-
-                    //создаю массив, в конце выполнения функции он удалится
+    //создаю массив, в конце выполнения функции он удалится
     auto T = new float* [lenX];
+    //Grid T;
     for (int i = 1; i < lenX - 1; i++)
     {
         T[i] = new float[lenX];
         for (int j = 1; j < lenX - 1; j++)
         {
+            //put(T, i, j, i / (2 * j) + pow(2, j));
             T[i][j] = i / (2 * j) + pow(2, j);
             //T[i][j] = 0.25 * (T[i + 1][j] + T[i - 1][j] + T[i][j + 1] + T[i][j - 1]);
         }
     }
     
-
-    Grid Lig;
-
-    //int d = get(Lig, 4, 6);  //test
-
-
-    for (int k = 2; k < lenX - 2; k++)
+    for (int i = 2; i < lenX - 2; i++)
     {
-        for (int l = 2; l < lenX - 2; l++)
+        for (int j = 2; j < lenX - 2; j++)
         {
-            float Z = (T[k - 1][l] + T[k + 1][l] + T[k][l - 1] + T[k][l + 1]);
+            float Z = (T[i - 1][j] + T[i + 1][j] + T[i][j - 1] + T[i][j + 1]);
+            //float Z = (get(T, i - 1, j) + get(T, i + 1, j) + get(T, i, j - 1) + get(T, i, j + 1));
 
             // определяю для x + 1 
-            //комменью пока else т к д быть false, если не задано иное
-            if (random_decision(k + 1, l, Z, n, T[k + 1][l])) { //&T[k + 1][l] почему надо так, а не : *T[k + 1][l]
+            //комменьтю пока else т к д быть false, если не задано иное
 
-                if ((get(Lig, k, l) == 1) or (get(Lig, k + 1, l - 1) == 1) or (get(Lig, k + 1, l + 1) == 1) or (get(Lig, k + 2, l) == 1)) {
+            int k = i + 1, l = j;
+            float z1 = T[k - 1][l] + T[k + 1][l] + T[k][l - 1] + T[k][l + 1];
+            if (random_decision(Z, n, z1)) { //&T[k + 1][l] почему надо так, а не : *T[k + 1][l]
 
-                    put(Lig, k + 1, l, true);
+                if ((get(Lig, i, j) == 1) or (get(Lig, i + 1, j - 1) == 1) or (get(Lig, i + 1, j + 1) == 1) or (get(Lig, i + 2, j) == 1)) {
+
+                    put(Lig, i + 1, j, true);
                 }
                 /*else {
                     get(Lig, k + 1, l) = 0;
@@ -130,9 +118,11 @@ Grid& generate_field(float n, Grid& grid) //было int вместо Grid& (mag
 
 
             // определяю для x - 1
-            if (random_decision(k - 1, l, Z, n, T[k - 1][l])) {
-                if ((get(Lig, k, l) == 1) or (get(Lig, k - 1, l - 1) == 1) or (get(Lig, k - 1, l + 1) == 1) or (get(Lig, k - 2, l) == 1)) {
-                    put(Lig, k - 1, l, true);
+            k = i - 1, l = j;
+            z1 = T[k - 1][l] + T[k + 1][l] + T[k][l - 1] + T[k][l + 1];
+            if (random_decision(Z, n, z1)) {
+                if ((get(Lig, i, j) == 1) or (get(Lig, i - 1, j - 1) == 1) or (get(Lig, i - 1, j + 1) == 1) or (get(Lig, i - 2, j) == 1)) {
+                    put(Lig, i - 1, j, true);
                 }
                 /*else {
                 get(Lig, k - 1][l] = 0;
@@ -144,9 +134,11 @@ Grid& generate_field(float n, Grid& grid) //было int вместо Grid& (mag
             }
 
             // определяю для y + 1 # (get(Lig, k - 1, l] == 0) & (get(Lig, k + 1, l] == 0) |
-            if (random_decision(k, l + 1, Z, n, T[k][l + 1])) {
-                if ((get(Lig, k, l) == 1) or (get(Lig, k - 1, l + 1) == 1) or (get(Lig, k, l + 2) == 1) or (get(Lig, k + 1, l + 1) == 1)) {
-                    put(Lig, k, l + 1, true);
+            k = i, l = j + 1;
+            z1 = T[k - 1][l] + T[k + 1][l] + T[k][l - 1] + T[k][l + 1];
+            if (random_decision(Z, n, z1)) {
+                if ((get(Lig, i, j) == 1) or (get(Lig, i - 1, j + 1) == 1) or (get(Lig, i, j + 2) == 1) or (get(Lig, i + 1, j + 1) == 1)) {
+                    put(Lig, i, j + 1, true);
                 }
                 /*else {
                 get(Lig, k][l + 1] = 0;
@@ -160,28 +152,36 @@ Grid& generate_field(float n, Grid& grid) //было int вместо Grid& (mag
     }
 
     //удаляю массив - очищаю память
-    for (size_t i = 0; i <= lenX; i++)
+    /*for (size_t i = 0; i <= lenX; i++)
     {
         delete T[i];
     }
-    delete T;
+    delete T;*/
 
-    return Lig;
-
+    //return Lig;
 }
 
 
-bool random_decision(int k, int l, float Z, float n, float &T) //float &T[k][l]
+int main()
 {
-    T->T[k][l];
+    cout << "Hello World!\n";
+    float n = 0.95;
+
+    Grid Lig;
+    Lig = *generate_field(n, Lig);
+    //int d = get(Lig, 4, 6);
+    //put(Lig, 5, 6, true); //тест работает ли
 
 
-    float psi = Z * rand();
-    float Z1 = pow((*T[k - 1][l] + *T[k + 1][l]  + *T[k][l - 1] + *T[k][l + 1]), n);
+    for (int i = 0; i < lenX; i++) {
+        cout << " \n";
+        for (int j = 0; j < lenX; j++) {
+            cout << get(Lig, i, j) + " ";
+        }
 
-    return Z1 > psi;
+    }
+
 }
-
 
 
 

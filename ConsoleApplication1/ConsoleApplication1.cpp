c80bc7dc0f320d7ae277f211d08527e488df529f
cs_mice <cs-maus-1@mail.ru>
2021-02-16 12:54:23 +0300

    работает, но надо починить удаление поля

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,8 +12,8 @@
 using namespace::std;
 
 //const unsigned int lenX = 3;
-int const lenX = 30;
-float n = 0.95;
+int const lenX = 35;
+float n = 0.97;
 
 
 double random(double min, double max)
@@ -26,13 +26,6 @@ double random(double min, double max)
 
     return min + (double)rand() / ((double)RAND_MAX / (max - min));
 }
-
-//double random(double min, double max)
-//{
-//    //srand(static_cast<unsigned int>(time(0))); // устанавливаем значение системных часов в качестве стартового числа
-//    srand(time(NULL));
-//    return (double)(rand()) / RAND_MAX * (max - min) + min;
-//}
 
 struct IntVector2 
 {
@@ -88,7 +81,7 @@ bool put(Grid& grid, int x, int y, bool it)
 }
 
 //сравниваем 
-bool random_decision(int k, int l, float Z, float n, float **T) //float &T[k][l]
+bool random_decision(int k, int l, float Z, float n, float **T)
 {
     float r = random(0, 1);
     float psi = r * Z;
@@ -100,16 +93,16 @@ bool random_decision(int k, int l, float Z, float n, float **T) //float &T[k][l]
 }
 
 
-//генератор поля (работает неверно)
-void generate_field(float n, Grid& varLig) //было int вместо Grid& (magic  @_@ )
+//генератор поля
+void generate_field(float n, Grid& varLig, int numIter)
 {
     //создаю массив, в конце выполнения функции он удалится
     auto T = new float*[lenX];
 
     int Ttop = 45;  // 100
     int Tbottom = 5; // 0
-    int Tleft = 1;  // 0
-    int Tright = 15;  // 30
+    int Tleft = 5;  // 0
+    int Tright = 5;  // 30
 
     //записываю в потенциальное (температурное) поле нули
     for (int i = 0; i < lenX; i++)
@@ -122,92 +115,69 @@ void generate_field(float n, Grid& varLig) //было int вместо Grid& (ma
     }
 
     //задаю граничные условия для поля потенциалов
-    for (int i = 0; i < lenX; i++)
+    for (int i = 1; i < lenX-1; i++)
     {
-        T[i][lenX - 1] = Ttop;
+        T[i][lenX - 2] = Ttop;
         T[i][0] = int(Tbottom * random(0, i));
-        T[lenX - 1][i] = Tright;
+        T[lenX - 2][i] = Tright;
         T[0][i] = Tleft;
     }
 
-    //рассчитываю градиент поля
-    int maxIter = 5;
-    for (size_t i = 0; i < maxIter; i++)
-    {
-        for (int i = 1; i < lenX - 1; i++)
-        {
-            //cout << "\n ";
-            for (int j = 1; j < lenX - 1; j++)
-            {
-                //T[i][j] = (i + 1) / ((float)(2) * (j + 1)) + pow(2, j);
-                T[i][j] = (0.25 * T[i + 1][j] + 0.25 * T[i - 1][j] + 0.25 * T[i][j + 1] + 0.25 * T[i][j - 1]);
-
-                //cout << int(T[i][j]);
-                //cout << "  ";
-
-            }
-        }
-    }
-
+    //рассчитываю поле
     for (int i = 2; i < lenX - 2; i++)
     {
+        //cout << "\n ";
         for (int j = 2; j < lenX - 2; j++)
         {
-            float Z = (T[i - 1][j] + T[i + 1][j] + T[i][j - 1] + T[i][j + 1]);
-            
-            //__________________НАЧИНАЕТСЯ КОД ДЛЯ ЗАДАНИЯ ПОЛЯ Lig____________________________
-            /*if (numIter == 0)
-                put(randPrint, i, j, random_decision(i + 1, j, Z, n, T));
-            else
-                put(randPrint2, i, j, random_decision(i + 1, j, Z, n, T));*/
+            T[i][j] = (i + 1) / ((float)(2) * (j + 1)) + pow(3, j)/ pow(2, j); //(pow(lenX, 2) + j + 1)
+            //T[i][j] = (0.25 * T[i + 1][j] + 0.25 * T[i - 1][j] + 0.25 * T[i][j + 1] + 0.25 * T[i][j - 1]);
+            //cout << int(T[i][j]);
+            //cout << "  ";
+        }
+    }
 
-            bool r_ip1 = random_decision(i + 1, j, Z, n, T) & (get(varLig, i, j) or get(varLig, i + 1, j - 1) or get(varLig, i + 1, j + 1) or get(varLig, i + 2, j));
-            bool r_im1 = random_decision(i - 1, j, Z, n, T) & (get(varLig, i, j) or get(varLig, i - 1, j - 1) or get(varLig, i - 1, j + 1) or get(varLig, i - 2, j));
-            bool r_jp1 = random_decision(i, j + 1, Z, n, T) & (get(varLig, i, j) or get(varLig, i - 1, j + 1) or get(varLig, i, j + 2) or get(varLig, i + 1, j + 1));
+    for (int i = 0; i < numIter; i++)
+    {
+        for (int i = 2; i < lenX - 2; i++)
+        {
+            for (int j = 2; j < lenX - 2; j++)
+            {
+                float Z = (T[i - 1][j] + T[i + 1][j] + T[i][j - 1] + T[i][j + 1]);
 
-            if (r_ip1) {
-                put(varLig, i + 1, j, true);
-                /*}
+                //__________________НАЧИНАЕТСЯ КОД ДЛЯ ЗАДАНИЯ ПОЛЯ Lig____________________________
+
+                bool r_ip1 = random_decision(i + 1, j, Z, n, T) & (get(varLig, i, j) or get(varLig, i + 1, j - 1) or get(varLig, i + 1, j + 1) or get(varLig, i + 2, j));
+                bool r_im1 = random_decision(i - 1, j, Z, n, T) & (get(varLig, i, j) or get(varLig, i - 1, j - 1) or get(varLig, i - 1, j + 1) or get(varLig, i - 2, j));
+                bool r_jp1 = random_decision(i, j + 1, Z, n, T) & (get(varLig, i, j) or get(varLig, i - 1, j + 1) or get(varLig, i, j + 2) or get(varLig, i + 1, j + 1));
+
+                if (r_ip1) {
+                    put(varLig, i + 1, j, true);
+                }
                 else {
                     put(varLig, i + 1, j, false);
-                }*/
-            }
-            else {
-                put(varLig, i + 1, j, false);
-            }
+                }
 
-            // определяю для x - 1
-            if (r_im1) {
-                //if (get(varLig, i, j) or get(varLig, i - 1, j - 1) or get(varLig, i - 1, j + 1) or get(varLig, i - 2, j)) {
-                put(varLig, i - 1, j, true);
-                /*}
+                // определяю для x - 1
+                if (r_im1) {
+                    put(varLig, i - 1, j, true);
+                }
                 else {
                     put(varLig, i - 1, j, false);
-                }*/
-            }
-            else {
-                put(varLig, i - 1, j, false);
-            }
+                }
 
-            // определяю для y + 1 # (get(Lig, k - 1, l] == 0) & (get(Lig, k + 1, l] == 0) |
-            if (r_jp1) {
-                put(varLig, i, j + 1, true);
-                /*}
+                // определяю для y + 1
+                if (r_jp1) {
+                    put(varLig, i, j + 1, true);
+                }
                 else {
                     put(varLig, i, j + 1, false);
-                }*/
-            }
-            else if ((!r_jp1) & (get(varLig, i + 1, j) or get(varLig, i - 1, j))) {
-                put(varLig, i, j + 1, false);
-            }
-            else if (!get(varLig, i + 1, j) & !get(varLig, i - 1, j)) {
-                put(varLig, i, j + 1, true);
+                }
             }
         }
     }
-    
 
-    //удаляю массив - очищаю память
+
+    //удаляю массив - очищаю память //пока не работает
     /*for (size_t i = 0; i <= lenX; i++)
     {
         delete T[i];
@@ -237,13 +207,10 @@ int main()
     //        put(Lig, i, rj * (lenX - nn) + j, true);
     //    }
     //}
-    int iter = int(lenX/4);
-    for (size_t i = 0; i < iter; i++)
-    {
-        put(Lig, 1, iter, true); //было 3 до замены на int(lenX* random(0.1, 1))
-        put(Lig, iter, 1, true);
-        generate_field(n, Lig);
-    }
+    int numIter = 2 * lenX;
+    put(Lig, 1, 3, true); //было 3 до замены на int(lenX* random(0.1, 1))
+    put(Lig, 3, 1, true);
+    generate_field(n, Lig, numIter);
 
 
     /*
@@ -296,17 +263,3 @@ int main()
         }
     }
 }
-
-
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
